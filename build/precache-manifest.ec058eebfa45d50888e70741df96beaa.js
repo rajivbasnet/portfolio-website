5ae7b7a9b7e7278b7dd8d80de43eff4e940810d3
rajivbasnet <net.rajivbas@gmail.com>
2020-06-19 14:19:41 -0400

    build

---
+++ build/precache-manifest.ec058eebfa45d50888e70741df96beaa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c365b4b23057d7009d522779dfd950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4250d862629b1cd11208",
+    "url": "/static/css/2.e22647b5.chunk.css"
+  },
+  {
+    "revision": "916561995d2e3955bde8",
+    "url": "/static/css/main.63e77a6b.chunk.css"
+  },
+  {
+    "revision": "4250d862629b1cd11208",
+    "url": "/static/js/2.ca6e2fe9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ca6e2fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916561995d2e3955bde8",
+    "url": "/static/js/main.385607d3.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "8bfbc526553ce0851c021fe99f23ffe9",
+    "url": "/static/media/Arizonia-Regular.8bfbc526.ttf"
+  },
+  {
+    "revision": "b39ffa3879dd8986c9e481d0e4de23da",
+    "url": "/static/media/bgimage1.b39ffa38.jpg"
+  },
+  {
+    "revision": "cea94f6852320b1df0912897199f14c9",
+    "url": "/static/media/bgimage2.cea94f68.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "41ad06aa1ba1535f6da0e34e9208b1f9",
+    "url": "/static/media/profilepic.41ad06aa.jpg"
+  }
+]);
